e8454e0f21c5b38aee23c0963a994828283e31ce
colinmorita <cmorita23@punahou.edu>
2016-06-30 11:58:32 -1000

    I kept going on the random word generator

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var verbs = ["eating","hitting","biting","smelling", "bouncing", "walking", "watching", "filming", "harvesting", "instructing", "zooming", "blushing", "shading", "hoping", "kicking", "closing", "haunting", "wriggling", "slapping", "begging"]
+var adjectives = ["likeable", "tall", "happy", "abath", "talented", "well-made", "tame", "simplistic", "illegal", "sticky", "quiet", "undesirable", "third", "far", "statuesque", "erect", "wrathful", "orange", "different", "coherent", "mountainous", "real", "well-groomed", "puny"]
+var nouns = ["vacation", "umbrella", "judge", "fog", "roll", "pickle", "zoo", "bell", "need", "baby", "lip", "book", "channel", "whale", "popsicle", "crow", "rabbit", "computer", "tree" "cactus", "bird", "falcon", "cookie", "robot", "server", "sign", "number", "fish", "frog", "dragon"]
+
+var verbsAmt = verbs[Math.floor(Math.random() * verbs.length-12;
+var adjectivesAmt = adjectives[Math.floor(Math.random() * adjectives.length-12;
+var nounsAmt = nouns[Math.floor(Math.random() * nouns.length)];
+var combos = verbsAmt * adjectivesAmt * nounsAmt;
+
+function UpdateName() {
+	Document.getElementById
+	
+	verbs = verbs[Math.floor(Math.random() * verbs.length)];
+	adjectives = adjectives[Math.floor(Math.random() * adjectives.length)];
+	nouns = nouns[Math.floor(Math.random() * nouns.length)];
